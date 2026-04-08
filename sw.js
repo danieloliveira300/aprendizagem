4be1f647268f21cadc95e7dd63a377a960b90bb4
danieloliveira300 <danielgaleno01@gmail.com>
2026-04-08 06:59:35 -0300

    Expand cache list in service worker
    Added additional files to cache for offline access.

--- sw.js
+++ sw.js
@@ -1,4 +1,38 @@
+const CACHE_NAME = 'aventura-saber-v1';
+// Lista de todos os ficheiros do seu projeto
 const urlsToCache = [
   './',
-  './index.html'
+  './index.html',
+  './img/apple.png',
+  './img/banana.png',
+  './img/circle.png',
+  './img/grape.png',
+  './img/orange.png',
+  './img/rectangle.png',
+  './img/square.png',
+  './img/star.png',
+  './img/strawberry.png',
+  './img/triangle.png',
+  './audio/chime.mp3',
+  './audio/soft_pop.mp3'
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Ficheiros guardados em cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devolve o ficheiro em cache se existir, senão vai à internet/servidor
+        return response || fetch(event.request);
+      })
+  );
+});
